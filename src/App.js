3286ed3787e5c0f7886c256e365e0dd28ae1a0cd
pinar <pnar.tunc@hotmail.com>
2023-11-13 18:20:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from "./components/TodoList";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
